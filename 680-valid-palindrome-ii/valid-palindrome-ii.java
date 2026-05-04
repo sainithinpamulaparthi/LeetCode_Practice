05a2e39dc3006b65332e4c3d4b00797d08e2f545
Sai Nithin Pamulaparthi <161051385+sainithinpamulaparthi@users.noreply.github.com>
2026-05-04 15:55:04 +0530

    Time: 4 ms (99.65%) | Memory: 47.6 MB (84.78%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+            return CP(s,i+1,j) || CP(s,i,j-1);
+            i++;
+            j--;
+        }
+        return true;
+    }
+    boolean CP(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
