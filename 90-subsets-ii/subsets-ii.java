63d6228a781523f26e5fe65aef941fbb91239927
Sai Nithin Pamulaparthi <161051385+sainithinpamulaparthi@users.noreply.github.com>
2026-05-06 14:50:46 +0530

    Time: 3 ms (56.16%) | Memory: 45 MB (76.04%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,20 @@
 class Solution {
-    List<List<Integer>> res =new ArrayList<>();
-    List<Integer> temp=new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Backtrack(0,nums);
+        backtrack(0, nums);
         return res;
     }
-     private void Backtrack(int idx,int[] nums){
+    private void backtrack(int idx, int[] nums) {
         res.add(new ArrayList<>(temp));
-        for(int i=idx;i<nums.length;i++){
-            if(i>idx && nums[i]==nums[i-1]) continue;
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) {
+                continue;
+            }
             temp.add(nums[i]);
-            Backtrack(i+1,nums);
-            temp.remove(temp.size()-1);
+            backtrack(i + 1, nums);
+            temp.remove(temp.size() - 1);
         }
     }
 }
