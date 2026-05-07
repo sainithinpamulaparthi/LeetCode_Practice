419bd08a82c51531af7a489592e552d233f5077a
Sai Nithin Pamulaparthi <161051385+sainithinpamulaparthi@users.noreply.github.com>
2026-05-07 14:36:29 +0530

    Time: 3 ms (87.59%) | Memory: 43.4 MB (25.82%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> st=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(ch=='('||ch=='['||ch=='{')
+            st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                else{
+                    char top=st.pop();
+                    if(ch==')'&&top!='(') return false;
+                    if(ch==']'&&top!='[') return false;
+                    if(ch=='}'&&top!='{') return false;
+                }
+            }
+       }
+        return st.isEmpty();
+    }
+}
